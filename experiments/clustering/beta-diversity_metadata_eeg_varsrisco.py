091d1e52da9791607cdb960f047f86a41777089f
davips <dpsabc@gmail.com>
2023-06-10 17:18:18 -0300

    Draft of predictor

---
+++ experiments/clustering/beta-diversity_metadata_eeg_varsrisco.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from numpy import mean, std, quantile
+from sklearn.manifold import TSNE
+from sortedness.local import sortedness
+
+from germina.config import local_cache_uri
+from germina.data import clean
+from hdict import _, apply, cache
+from shelchemy import sopen
+
+files = [
+    ("data_microbiome___2023-05-10___beta_diversity_distance_matrix_T1.csv", None),
+    ("data_eeg___2023-03-15___VEP-N1---covariates-et-al---Average-et-al.csv", None),
+    ("metadata___2023-05-08-fup5afixed.csv", ["id_estudo", "ibq_reg_t1", "ibq_reg_t2"]),
+    ("nathalia170523_variaveis_risco___2023-06-08.csv", None)
+]
+targets = ["risco_class", "ibq_reg_t1", "ibq_reg_t2", "ibq_reg_t2-ibq_reg_t1"]
+d = clean(targets, "data/", files, [local_cache_uri])
+cm = "coolwarm"
+for target in targets:
+    if "-" in target:
+        a, b = target.split("-")
+        labels = d.targets[a] - d.targets[b]
+        labels = np.where(labels <= -1, -3, labels)
+        # labels = np.where((labels > -1) & (labels < 1), 0, labels)
+        # labels = np.where(labels >= 1, 1, labels)
+        labels *= -1
+        scale = 150
+    else:
+        labels = d.targets[target]
+        if target.startswith("ibq_reg_t"):
+            qmn, qmx = quantile(labels, [1 / 4, 3 / 4])
+            menores = labels[labels <= qmn].index
+            maiores = labels[labels >= qmx].index
+            pd.options.mode.chained_assignment = None
+            labels.loc[menores] = -1
+            labels.loc[maiores] = 1
+            labels.loc[list(set(labels.index).difference(maiores, menores))] = 0
+            pd.options.mode.chained_assignment = "warn"
+            scale = 150
+        else:
+            labels = np.where(labels == 0, 20, labels)
+            labels = np.where(labels == 1, 10, labels)
+            labels = np.where(labels == 2, 30, labels)
+            scale = 200
+    mn, mx = min(labels), max(labels)
+    for ndims in [2, 3][:]:
+        with sopen(local_cache_uri) as local:  # , sopen(remote_cache_uri) as remote:
+            d >>= (
+                    apply(TSNE, n_components=ndims, random_state=0, method='exact', n_jobs=-1).tsne >> apply(TSNE.fit_transform, _.tsne, X=_.p).r
+                    >> apply(sortedness, _.df, _.r).q
+                    >> cache(local)
+            )
+        r, q = d.r, d.q
+        print(f"{target} {ndims}d".ljust(25), mean(q), std(q), sep="\t")
+        t0 = -1
+        t1 = 0.1
+        bad = (q >= t0) & (q <= t1)
+        sizes = np.where(bad, 10, q * scale)
+        colors = labels
+        if ndims == 2:
+            fig = plt.figure()
+            plt.title(f"{target} ({ndims})")
+            ax = fig.add_subplot(111)
+            ax.scatter(r[:, 0], r[:, 1], vmin=mn, vmax=mx, c=colors, cmap=cm, s=sizes)
+            if "-" in target:
+                mask = colors == 3
+                colors = colors[mask]
+                sizes = sizes[mask]
+                # mask = mask.reshape(-1, 1) # & [True, True]
+                r = r[mask, :]
+            ax.scatter(r[:, 0], r[:, 1], vmin=mn, vmax=mx, c=colors, cmap=cm, s=sizes)
+        elif ndims == 3:
+            fig = plt.figure()
+            plt.title(f"{target} ({ndims})")
+            ax = fig.add_subplot(111, projection="3d")
+            ax.scatter(r[:, 0], r[:, 1], r[:, 2], vmin=mn, vmax=mx, c=colors, cmap=cm, s=sizes)
+
+plt.show()
--- experiments/prediction/beta-diversity_metadata_eeg_varsrisco.py
+++ experiments/prediction/beta-diversity_metadata_eeg_varsrisco.py
@@ -1,3 +1,4 @@
+from sklearn.ensemble import RandomForestClassifier as RF
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -24,10 +25,6 @@ for target in targets:
         a, b = target.split("-")
         labels = d.targets[a] - d.targets[b]
         labels = np.where(labels <= -1, -3, labels)
-        # labels = np.where((labels > -1) & (labels < 1), 0, labels)
-        # labels = np.where(labels >= 1, 1, labels)
-        labels *= -1
-        scale = 150
     else:
         labels = d.targets[target]
         if target.startswith("ibq_reg_t"):
@@ -38,44 +35,16 @@ for target in targets:
             labels.loc[menores] = -1
             labels.loc[maiores] = 1
             labels.loc[list(set(labels.index).difference(maiores, menores))] = 0
-            pd.options.mode.chained_assignment = "warn"
-            scale = 150
         else:
             labels = np.where(labels == 0, 20, labels)
             labels = np.where(labels == 1, 10, labels)
             labels = np.where(labels == 2, 30, labels)
-            scale = 200
-    mn, mx = min(labels), max(labels)
-    for ndims in [2, 3][:]:
-        with sopen(local_cache_uri) as local:  # , sopen(remote_cache_uri) as remote:
-            d >>= (
-                    apply(TSNE, n_components=ndims, random_state=0, method='exact', n_jobs=-1).tsne >> apply(TSNE.fit_transform, _.tsne, X=_.p).r
-                    >> apply(sortedness, _.df, _.r).q
-                    >> cache(local)
-            )
-        r, q = d.r, d.q
-        print(f"{target} {ndims}d".ljust(25), mean(q), std(q), sep="\t")
-        t0 = -1
-        t1 = 0.1
-        bad = (q >= t0) & (q <= t1)
-        sizes = np.where(bad, 10, q * scale)
-        colors = labels
-        if ndims == 2:
-            fig = plt.figure()
-            plt.title(f"{target} ({ndims})")
-            ax = fig.add_subplot(111)
-            ax.scatter(r[:, 0], r[:, 1], vmin=mn, vmax=mx, c=colors, cmap=cm, s=sizes)
-            if "-" in target:
-                mask = colors == 3
-                colors = colors[mask]
-                sizes = sizes[mask]
-                # mask = mask.reshape(-1, 1) # & [True, True]
-                r = r[mask, :]
-            ax.scatter(r[:, 0], r[:, 1], vmin=mn, vmax=mx, c=colors, cmap=cm, s=sizes)
-        elif ndims == 3:
-            fig = plt.figure()
-            plt.title(f"{target} ({ndims})")
-            ax = fig.add_subplot(111, projection="3d")
-            ax.scatter(r[:, 0], r[:, 1], r[:, 2], vmin=mn, vmax=mx, c=colors, cmap=cm, s=sizes)
 
-plt.show()
+    with sopen(local_cache_uri) as local:  # , sopen(remote_cache_uri) as remote:
+        d >>= (
+                apply(RF, n_estimators=1000, random_state=0, n_jobs=-1).rf
+                >> apply(RF.fit, _.rf, X=_.p, y=labels).none
+                >> apply(RF.predict, _.rf, X=_.p).r
+                >> cache(local)
+        )
+        print(d.r)
