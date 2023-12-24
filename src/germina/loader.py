1f7d4056392dbf6f1298a2f502531bc62d6ffe7a
davips <dpsabc@gmail.com>
2023-12-24 15:38:38 -0300

    Fix fix

--- src/germina/loader.py
+++ src/germina/loader.py
@@ -138,10 +138,9 @@ def clean_for_dalex(d, storages, storage_to_be_updated, verbose=False, target="E
     else:
         d = d >> apply(cut).y
         d = ch(d, storages, storage_to_be_updated)
-        d.apply(lambda X, y: X[y.index], out=f"X")
+        d.apply(lambda X, y: X.loc[y.index], out=f"X")
     d = ch(d, storages, storage_to_be_updated)
-    d.apply(lambda df, alias, X, y: df[alias][y.index], alias=alias, out=f"Xor_{field}_{alias}")
-    d.apply(lambda df, alias, y: df[alias][y.index], alias=alias, out=f"yor_{field}_{alias}")
+    d.apply(lambda df, alias, y: df[alias].loc[y.index], alias=alias, out=f"yor_{field}_{alias}")
     if verbose:
         print("Cleaned ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓", d.X.shape, d.y.shape, "↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑\n")
     return d
