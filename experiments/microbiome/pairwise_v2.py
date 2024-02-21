Fix bug : leakage when filtering pairs at prediction
Add extreme flag

experiments/microbiome/pairwise_v2.py
@@ -11,9 +11,9 @@ from shelchemy import sopen
 from germina.config import local_cache_uri, remote_cache_uri, near_cache_uri, schedule_uri
 from germina.loo import loo
 
-dct = handle_command_line(argv, noage=False, delta=float, trees=int, pct=False, demo=False, sched=False, perms=1, targetvar=str, jobs=int, alg=str, seed=0, prefix=str, sufix=str, trees_imp=int, feats=int, tfsel=int, forward=False, pairwise=str, sps=list, center=float)
+dct = handle_command_line(argv, noage=False, delta=float, trees=int, pct=False, demo=False, sched=False, perms=1, targetvar=str, jobs=int, alg=str, seed=0, prefix=str, sufix=str, trees_imp=int, feats=int, tfsel=int, forward=False, pairwise=str, sps=list, center=float, x=False)
 print(dct)
-noage, trees, delta, pct, demo, sched, perms, targetvar, jobs, alg, seed, prefix, sufix, trees_imp, feats, tfsel, forward, pairwise, sps, center = dct["noage"], dct["trees"], dct["delta"], dct["pct"], dct["demo"], dct["sched"], dct["perms"], dct["targetvar"], dct["jobs"], dct["alg"], dct["seed"], dct["prefix"], dct["sufix"], dct["trees_imp"], dct["feats"], dct["tfsel"], dct["forward"], dct["pairwise"], dct["sps"], dct["center"]
+noage, trees, delta, pct, demo, sched, perms, targetvar, jobs, alg, seed, prefix, sufix, trees_imp, feats, tfsel, forward, pairwise, sps, center, x = dct["noage"], dct["trees"], dct["delta"], dct["pct"], dct["demo"], dct["sched"], dct["perms"], dct["targetvar"], dct["jobs"], dct["alg"], dct["seed"], dct["prefix"], dct["sufix"], dct["trees_imp"], dct["feats"], dct["tfsel"], dct["forward"], dct["pairwise"], dct["sps"], dct["center"], dct["x"]
 rnd = np.random.default_rng(0)
 with (sopen(local_cache_uri, ondup="skip") as local_storage, sopen(near_cache_uri, ondup="skip") as near_storage, sopen(remote_cache_uri, ondup="skip") as remote_storage, sopen(schedule_uri) as db):
     storages = {
@@ -34,7 +34,7 @@ with (sopen(local_cache_uri, ondup="skip") as local_storage, sopen(near_cache_ur
             del df["idade_crianca_dias_t2"]  #####################################
         # df = df[["idade_crianca_dias_t2", "bayley_8_t2"]]
         print(df.shape, "<<<<<<<<<<<<<<<<<")
-        ret = loo(df, permutation=0, pairwise=pairwise, threshold=delta, rejection_threshold=0,
+        ret = loo(df, permutation=0, pairwise=pairwise, threshold=delta, rejection_threshold=0, extreme_pairing_onprediction=x,
                   alg=alg, n_estimators=trees,
                   n_estimators_imp=trees_imp,
                   n_estimators_fsel=tfsel, forward_fsel=forward, k_features_fsel=feats, k_folds_fsel=4,
@@ -49,7 +49,7 @@ with (sopen(local_cache_uri, ondup="skip") as local_storage, sopen(near_cache_ur
         for permutation in ap[1, 2, ..., perms]:
             df_shuffled = df.copy()
             df_shuffled[targetvar] = rnd.permutation(df[targetvar].values)
-            ret = loo(df_shuffled, permutation, pairwise=pairwise, threshold=delta, rejection_threshold=0,
+            ret = loo(df_shuffled, permutation, pairwise=pairwise, threshold=delta, rejection_threshold=0, extreme_pairing_onprediction=x,
                       alg=alg, n_estimators=trees,
                       n_estimators_imp=trees_imp,
                       n_estimators_fsel=tfsel, forward_fsel=forward, k_features_fsel=feats, k_folds_fsel=4,

src/germina/pairwiserf.py
@@ -21,13 +21,13 @@ class PairwiseClassifier(BaseEstimator, ClassifierMixin):
     0.717211440...
     >>> c = b.reshape(len(b), 1)
     >>> X = np.hstack([a, c])
-    >>> alg = PairwiseClassifier(threshold=20, threshold_interpolation=False, center=me, random_state=0, n_jobs=-1)
+    >>> alg = PairwiseClassifier(threshold=20, extreme_pairing=False, center=me, random_state=0, n_jobs=-1)
     >>> np.mean(cross_val_score(alg, X, y, cv=StratifiedKFold()))  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
     0.748901940...
     """
 
     def __init__(self, n_estimators=100, random_state=None,
-                 pairwise="concatenation", threshold_interpolation=False,
+                 pairwise="concatenation", extreme_pairing=False,
                  pct=False, threshold=0, center=0, n_jobs=1):
         self.n_estimators = n_estimators
         self.n_jobs = n_jobs
@@ -35,7 +35,7 @@ class PairwiseClassifier(BaseEstimator, ClassifierMixin):
         self.pairwise = pairwise
         self.threshold = threshold
         self.pct = pct
-        self.threshold_interpolation = threshold_interpolation
+        self.extreme_pairing = extreme_pairing
         self.center = center
         # TODO: complete all alg args here
         self.alg = LGBMc(n_estimators=n_estimators, random_state=random_state, n_jobs=n_jobs, deterministic=True, force_row_wise=True)
@@ -88,7 +88,7 @@ class PairwiseClassifier(BaseEstimator, ClassifierMixin):
             ytr = (w >= self.center).astype(int)
 
         self.alg.fit(Xtr, ytr)
-        self.Xw_tr = self.Xw[abs(self.Xw[:, -1] - self.center) >= self.threshold] if self.threshold_interpolation else self.Xw
+        self.Xw_tr = self.Xw[abs(self.Xw[:, -1] - self.center) >= self.threshold] if self.extreme_pairing else self.Xw
         return self
 
     def predict(self, X):
@@ -98,7 +98,6 @@ class PairwiseClassifier(BaseEstimator, ClassifierMixin):
         X = Xw_ts[:, :-1]  # discard label to avoid data leakage
 
         handle_last_as_y = "%" if self.pct else True
-        filter = lambda tmp: (tmp < -self.threshold) | (tmp >= self.threshold)
         pairwise = True
         if self.pairwise == "difference":
             pairs = lambda a, b: pairwise_diff(a, b)