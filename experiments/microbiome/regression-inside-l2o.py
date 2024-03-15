fc01b3030fe24eb0969180c1a1ee9532f1f45513
davips <dpsabc@gmail.com>
2024-03-15 14:21:46 -0300

    Evaluate by either r2,bacc

--- experiments/microbiome/regression-inside-l2o.py
+++ experiments/microbiome/regression-inside-l2o.py
@@ -20,15 +20,14 @@ from germina.config import local_cache_uri, remote_cache_uri, near_cache_uri, sc
 from germina.runner import ch
 from germina.sampling import pairwise_sample
 from germina.shaps import SHAPs
-from germina.trees import tree_optimized_dv
+from germina.trees import tree_optimized_dv_pair
 
 center, max_trials = None, 10_000_000
-dct = handle_command_line(argv, batches=int, cache=False, font=12, alg=str, demo=False, delta=20, noage=False, sched=False, targetvar=str, jobs=int, seed=0, prefix=str, suffix=str, sps=list, nsamp=int, shap=False, tree=False)
+dct = handle_command_line(argv, r2=False, batches=int, cache=False, font=12, alg=str, demo=False, delta=20, noage=False, sched=False, targetvar=str, jobs=int, seed=0, prefix=str, suffix=str, sps=list, nsamp=int, shap=False, tree=False)
 print(dct)
-batches, cache, font, alg, demo, delta, noage, sched, targetvar, jobs, seed, prefix, suffix, sps, nsamp, shap, tree = dct["batches"], ["cache"], dct["font"], dct["alg"], dct["demo"], dct["delta"], dct["noage"], dct["sched"], dct["targetvar"], dct["jobs"], dct["seed"], dct["prefix"], dct["suffix"], dct["sps"], dct["nsamp"], dct["shap"], dct["tree"]
+r2, batches, cache, font, alg, demo, delta, noage, sched, targetvar, jobs, seed, prefix, suffix, sps, nsamp, shap, tree = dct["r2"], dct["batches"], ["cache"], dct["font"], dct["alg"], dct["demo"], dct["delta"], dct["noage"], dct["sched"], dct["targetvar"], dct["jobs"], dct["seed"], dct["prefix"], dct["suffix"], dct["sps"], dct["nsamp"], dct["shap"], dct["tree"]
 rnd = np.random.default_rng(0)
-trials = int(alg.split("-")[1])
-kfolds0 = int(alg.split("-")[2])
+batch_size = int(alg.split("-")[1])
 
 with (sopen(local_cache_uri, ondup="skip") as local_storage, sopen(near_cache_uri, ondup="skip") as near_storage, sopen(remote_cache_uri, ondup="skip") as remote_storage, sopen(schedule_uri) as db):
     storages = {
@@ -55,32 +54,41 @@ with (sopen(local_cache_uri, ondup="skip") as local_storage, sopen(near_cache_ur
         if noage:
             del df[agevar]
         print(df.shape)
-        kfolds, kfolds_full = (df.shape[0] - 2, df.shape[0]) if kfolds0 == 0 else (kfolds0, kfolds0)
-        d = hdict(algname=alg, n_iter=max_trials, batchs=batches, demo=demo, columns=df.columns.tolist()[:-1], k=kfolds, shap=shap, seed=seed, _njobs_=jobs, _verbose_=True)
+        # kfolds, kfolds_full = (df.shape[0] - 2, df.shape[0]) if kfolds0 == 0 else (kfolds0, kfolds0)
+        d = hdict(algname=alg, trials=max_trials, demo=demo, columns=df.columns.tolist()[:-1], shap=shap, seed=seed, _njobs_=jobs, _verbose_=True)
 
         if tree:  ###############################################################################################################
-            best_score = -1000
+            best_r2 = best_bacc = -1000
             for batch in range(batches):
-                start = trials * batch
-                end = start + trials
+                start = batch_size * batch
+                end = start + batch_size
                 # noinspection PyTypeChecker
-                d.apply(tree_optimized_dv, df, start=start, end=end, k=kfolds_full, out="best")
+                d.apply(tree_optimized_dv_pair, df, start=start, end=end, njobs=_._njobs_, verbose=_._verbose_, out="best")
                 if cache:
                     d = ch(d, storages)
-                params, score = d.best
-                if score > best_score:
-                    best_score = score
-                    best_params = params
+                r2_params, bacc_params, r2, bacc = d.best
+                if r2 > best_r2:
+                    best_r2 = r2
+                    best_r2_params = r2_params
+                if bacc > best_bacc:
+                    best_bacc = bacc
+                    best_bacc_params = bacc_params
             # noinspection PyTypeChecker
-            d.apply(fit, alg, best_params, df, out="tree")
+            if r2:
+                # noinspection PyTypeChecker
+                d.apply(fit, alg, best_r2_params, df, out="tree")
+            else:
+                # noinspection PyTypeChecker
+                d.apply(fit, alg, best_bacc_params, df, out="tree")
             if cache:
                 d = ch(d, storages)
             best_estimator = d.tree
 
             print("vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv")
-            print(best_params)
+            print(best_r2_params)
+            print(best_bacc_params)
             print("--------------------------------")
-            print(best_score)
+            print(best_r2, best_bacc)
             print(" ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^")
             columns = df.columns.tolist()[:-1]
             plot_tree(best_estimator, filled=True, feature_names=columns, fontsize=font)
@@ -89,114 +97,123 @@ with (sopen(local_cache_uri, ondup="skip") as local_storage, sopen(near_cache_ur
             continue
 
         # L2O ##############################################################################################################
-        hits = {0: 0, 1: 0}
-        tot, errors = {0: 0, 1: 0}, {0: [], 1: []}
-        t, z = [], []
-        t_diff, z_diff = [], []
-        bacc = 0
-        shaps = SHAPs()
         ansi = d.hosh.ansi
-        pairs = pairwise_sample(df.index, nsamp, seed)
-        tasks = zip(repeat(f"{batches}×{targetvar} {noage=} {seed=} {sp=}"), repeat(alg), repeat(d.id), pairs)
-        for c, (targetvar0, alg0, did0, (idxa, idxb)) in enumerate((Scheduler(db, timeout=60) << tasks) if sched else tasks):
-            if not sched:
-                print(f"\r{ansi} {targetvar0, alg0, (idxa, idxb)}: {c:3} {100 * c / len(pairs):4.2f}% {bacc:5.3f}          ", end="", flush=True)
+        pairs = pairwise_sample(df.index.tolist(), nsamp, seed)
+        best_r2__dct, best_r2_params__dct = {}, {}
+        best_bacc__dct, best_bacc_params__dct = {}, {}
+        for until_batch in range(batches):
+            start = batch_size * until_batch
+            end = start + batch_size
 
-            # prepare current pair of babies and training set
-            babydfa = df.loc[[idxa], :]
-            babydfb = df.loc[[idxb], :]
-            baby_ya = babydfa.iloc[0, -1:]
-            baby_yb = babydfb.iloc[0, -1:]
-            # TODO remove babies with NaN labels in training set?
-            if baby_ya.isna().sum().sum() > 0 or baby_yb.isna().sum().sum() > 0:
-                continue  # skip NaN labels from testing set
-            baby_ya = baby_ya.to_numpy()
-            baby_yb = baby_yb.to_numpy()
-            babya = babydfa.to_numpy()
-            babyb = babydfb.to_numpy()
-
-            # optimize
-            Xw_tr = df.drop([idxa, idxb], axis="rows")
-            Xw_ts = np.vstack([babya, babyb])
-            best_score = -1000
-            for batch in range(batches):
-                start = trials * batch
-                end = start + trials
-                # noinspection PyTypeChecker
-                d.apply(tree_optimized_dv, Xw_tr, start=start, end=end, out="best")
-                if cache:
-                    d = ch(d, storages)
-                params, score = d.best
-                if score > best_score:
-                    best_score = score
-                    best_params = params
+            hits = {0: 0, 1: 0}
+            tot, errors = {0: 0, 1: 0}, {0: [], 1: []}
+            t, z = [], []
+            t_diff, z_diff = [], []
+            bacc = 0
+            shaps = SHAPs()
+            tasks = zip(repeat(f"{until_batch}/{batches}:{targetvar} {noage=} {seed=} {sp=}"), repeat(alg), repeat(d.id), pairs)
+            for c, (targetvar0, alg0, did0, (idxa, idxb)) in enumerate((Scheduler(db, timeout=60) << tasks) if sched else tasks):
                 if not sched:
                     print(f"\r{ansi} {targetvar0, alg0, (idxa, idxb)}: {c:3} {100 * c / len(pairs):4.2f}% {bacc:5.3f}          ", end="", flush=True)
 
-            # fit, predict
-            # noinspection PyTypeChecker
-            d.apply(fitpredict, alg, best_params, Xw_tr, Xw_ts[:, :-1], out="zts")
-            if cache:
-                d = ch(d, storages)
-            zts = d.zts
-            if not sched:
-                print(f"\r{ansi} {targetvar0, alg0, (idxa, idxb)}: {c:3} {100 * c / len(pairs):4.2f}% {bacc:5.3f}          ", end="", flush=True)
+                # prepare current pair of babies and training set
+                babydfa = df.loc[[idxa], :]
+                babydfb = df.loc[[idxb], :]
+                baby_ya = babydfa.iloc[0, -1:]
+                baby_yb = babydfb.iloc[0, -1:]
+                # TODO remove babies with NaN labels in training set?
+                if baby_ya.isna().sum().sum() > 0 or baby_yb.isna().sum().sum() > 0:
+                    continue  # skip NaN labels from testing set
+                baby_ya = baby_ya.to_numpy()
+                baby_yb = baby_yb.to_numpy()
+                babya = babydfa.to_numpy()
+                babyb = babydfb.to_numpy()
 
-            if shap:
-                raise Exception(f"")
+                # optimize  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                Xw_tr = df.drop([idxa, idxb], axis="rows")
+                Xw_ts = np.vstack([babya, babyb])
                 # noinspection PyTypeChecker
-                # d.apply(shap_for_pair, d.result_train["best_params"], babya, babyb, Xw_tr, jobs=_._jobs_, out="result_shap")
-                # d = ch(d, storages)
-                # shp = d.result_shap
-                # shaps.add(babya, babyb, shp)
+                d.apply(tree_optimized_dv_pair, Xw_tr, start=start, end=end, njobs=_._njobs_, verbose=_._verbose_, out="best")
+                if cache:
+                    d = ch(d, storages)
+                r2_params, bacc_params, r2, bacc = d.best
+                if (idxa, idxb) not in best_r2__dct or r2 > best_r2__dct[(idxa, idxb)]:
+                    best_r2__dct[(idxa, idxb)] = r2
+                    best_r2_params__dct[(idxa, idxb)] = r2_params
+                if (idxa, idxb) not in best_bacc__dct or bacc > best_bacc__dct[(idxa, idxb)]:
+                    best_bacc__dct[(idxa, idxb)] = bacc
+                    best_bacc_params__dct[(idxa, idxb)] = bacc_params
+                if not sched:
+                    print(f"\r{ansi} {targetvar0, alg0, (idxa, idxb)}: {c:3} {100 * c / len(pairs):4.2f}% {bacc:5.3f}          ", end="", flush=True)
+
+                # fit, predict +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                if r2:
+                    # noinspection PyTypeChecker
+                    d.apply(fitpredict, params=best_r2_params[(idxa, idxb)], Xwtr=Xw_tr, Xts=Xw_ts[:, :-1], out="zts")
+                else:
+                    # noinspection PyTypeChecker
+                    d.apply(fitpredict, params=best_bacc_params__dct[(idxa, idxb)], Xwtr=Xw_tr, Xts=Xw_ts[:, :-1], out="zts")
+                if cache:
+                    d = ch(d, storages)
+                zts = d.zts
+                if not sched:
+                    print(f"\r{ansi} {targetvar0, alg0, (idxa, idxb)}: {c:3} {100 * c / len(pairs):4.2f}% {bacc:5.3f}          ", end="", flush=True)
+
+                if shap:
+                    raise Exception(f"")
+                    # noinspection PyTypeChecker
+                    # d.apply(shap_for_pair, d.result_train["best_params"], babya, babyb, Xw_tr, jobs=_._jobs_, out="result_shap")
+                    # d = ch(d, storages)
+                    # shp = d.result_shap
+                    # shaps.add(babya, babyb, shp)
+
+                if sched:
+                    continue
+
+                # accumulate +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                pass
+                # expected = int(baby_ya[0] + delta >= baby_yb[0])
+                # predicted = int(zts[0] + delta >= zts[1])
+                # expected = int(abs(baby_ya[0] - baby_yb[0]) >= delta)
+                # predicted = int(abs(zts[0] - zts[1]) >= delta)
+                expected = int(baby_ya[0] >= baby_yb[0])
+                predicted = int(zts[0] >= zts[1])
+                # expected = int(baby_ya[0] / baby_yb[0] >= 1 + delta / 100)
+                # predicted = int(zts[0] / zts[1] >= 1 + delta / 100)
+                t.append(expected)
+                t_diff.append(baby_ya[0] - baby_yb[0])
+                tot[expected] += 1
+                z.append(predicted)
+                hits[expected] += int(expected == predicted)
+                z_diff.append(zts[0] - zts[1])
+
+                # errors +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                if expected != predicted:
+                    errors[expected].append((babydfa, babydfb))
+
+                # temporary accuracy +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                # print(baby_ya, baby_yb, expected, predicted)
+                if tot[0] * tot[1] > 0:
+                    acc0 = hits[0] / tot[0]
+                    acc1 = hits[1] / tot[1]
+                    bacc = (acc0 + acc1) / 2
 
             if sched:
                 continue
 
-            # accumulate
-            pass
-            # expected = int(baby_ya[0] + delta >= baby_yb[0])
-            # predicted = int(zts[0] + delta >= zts[1])
-            # expected = int(abs(baby_ya[0] - baby_yb[0]) >= delta)
-            # predicted = int(abs(zts[0] - zts[1]) >= delta)
-            expected = int(baby_ya[0] >= baby_yb[0])
-            predicted = int(zts[0] >= zts[1])
-            # expected = int(baby_ya[0] / baby_yb[0] >= 1 + delta / 100)
-            # predicted = int(zts[0] / zts[1] >= 1 + delta / 100)
-            t.append(expected)
-            t_diff.append(baby_ya[0] - baby_yb[0])
-            tot[expected] += 1
-            z.append(predicted)
-            hits[expected] += int(expected == predicted)
-            z_diff.append(zts[0] - zts[1])
+            # classification
+            if tot[0] == 0 or tot[1] == 0:
+                print(f"Resulted in class total with zero value: {tot=}")
+                bacc = -1
 
-            # errors
-            if expected != predicted:
-                errors[expected].append((babydfa, babydfb))
+            # precision_recall_curve
+            aps = average_precision_score(t, z) if bacc > 0 else None
+            pr, rc = precision_recall_curve(t, z)[:2]
+            auprc = auc(rc, pr) if bacc > 0 else None
+            r2 = r2_score(t_diff, z_diff) if bacc > 0 else None
+            tau = kendalltau(t_diff, z_diff)[0] if bacc > 0 else None
+            pea = correlation(t_diff, z_diff) if bacc > 0 else None
+            print(f"\r{sp=} {delta=} {hits=} {tot=} \t{d.hosh.ansi} | {bacc=:4.3f} | {aps=:4.3f} | {auprc=:4.3f} | "
+                  f"{r2=:4.3f} | {tau=:4.3f} | {pea=:4.3f}", flush=True)
 
-            # temporary accuracy
-            # print(baby_ya, baby_yb, expected, predicted)
-            if tot[0] * tot[1] > 0:
-                acc0 = hits[0] / tot[0]
-                acc1 = hits[1] / tot[1]
-                bacc = (acc0 + acc1) / 2
-
-        if sched:
-            continue
-
-        # classification
-        if tot[0] == 0 or tot[1] == 0:
-            print(f"Resulted in class total with zero value: {tot=}")
-            bacc = -1
-
-        # precision_recall_curve
-        aps = average_precision_score(t, z) if bacc > 0 else None
-        pr, rc = precision_recall_curve(t, z)[:2]
-        auprc = auc(rc, pr) if bacc > 0 else None
-        r2 = r2_score(t_diff, z_diff) if bacc > 0 else None
-        tau = kendalltau(t_diff, z_diff)[0] if bacc > 0 else None
-        pea = correlation(t_diff, z_diff) if bacc > 0 else None
-        print(f"\r{sp=} {delta=} {hits=} {tot=} \t{d.hosh.ansi} | {bacc=:4.3f} | {aps=:4.3f} | {auprc=:4.3f} | "
-              f"{r2=:4.3f} | {tau=:4.3f} | {pea=:4.3f}", flush=True)
-
-    print("\n")
+        print("\n")
--- src/germina/trees.py
+++ src/germina/trees.py
@@ -4,10 +4,11 @@ import numpy as np
 from joblib.parallel import Parallel, delayed
 from pairwiseprediction.classifier import PairwiseClassifier
 from pairwiseprediction.optimized import OptimizedPairwiseClassifier
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score, balanced_accuracy_score
 from sklearn.model_selection import RandomizedSearchCV, StratifiedKFold, ParameterSampler
 
-from germina.aux import predictors, get_algclass, get_algspace
+from germina.aux import predictors, get_algclass, get_algspace, fitpredict
+from germina.sampling import pairwise_sample
 
 
 def pwtree(df, alg, seed, jobs, pairwise, delta, proportion=False, center=None, only_relevant_pairs_on_prediction=False, verbose=False):
@@ -40,6 +41,7 @@ def tree_optimized(df, alg: RandomizedSearchCV, verbose=False):
     return alg.best_estimator_, alg.best_params_, alg.best_score_, alg.cv_results_
 
 
+# TODO: stratification is wrong as w is always positive
 def tree_optimized_dv(df, n_iter, start, end, k, algname, seed=0, njobs=16, verbose=False):
     if verbose:
         print("\tOptimizing reg. tree hyperparameters.", end="", flush=True)
@@ -79,3 +81,60 @@ def tree_optimized_dv(df, n_iter, start, end, k, algname, seed=0, njobs=16, verb
             best_score = score
             best_params = params
     return best_params.copy(), best_score
+
+
+def tree_optimized_dv_pair(df, trials, start, end, algname, seed=0, njobs=16, verbose=False):
+    """
+
+    :param df:
+    :param trials:  number of pairs
+    :param start:
+    :param end:
+    :param algname:
+    :param seed:
+    :param njobs:
+    :param verbose:
+    :return:
+    """
+    if verbose:
+        print("\tOptimizing reg by pair.", end="", flush=True)
+    search_space = get_algspace(algname)
+    pairs = pairwise_sample(df.index.tolist(), trials, seed)
+
+    def job(params_):
+        vts_lst, wts_lst = [], []  # continuous
+        yts_lst, zts_lst = [], []  # binary
+        for idxa, idxb in pairs:
+            # prepare current pair of babies for testing and build training set
+            babydfa = df.loc[[idxa], :]
+            babydfb = df.loc[[idxb], :]
+            baby_va = babydfa.iloc[0, -1:].to_numpy()
+            baby_vb = babydfb.iloc[0, -1:].to_numpy()
+            babya = babydfa.to_numpy()
+            babyb = babydfb.to_numpy()
+            Xv_tr = df.drop([idxa, idxb], axis="rows")
+            Xv_ts = np.vstack([babya, babyb])
+
+            # train/predict with sampled arguments
+            wts = fitpredict(algname, params_, Xv_tr, Xv_ts[:, :-1], verbose=False)
+
+            # accumulate
+            vts_lst.extend([baby_va[0], baby_vb[0]])
+            wts_lst.extend([wts[0], wts[1]])
+            yts_lst.append(baby_va[0] >= baby_vb[0])
+            zts_lst.append(wts[0] >= wts[1])
+        r2_ = r2_score(vts_lst, wts_lst)
+        bacc_ = balanced_accuracy_score(yts_lst, zts_lst)
+        return r2_, bacc_, params_
+
+    sampler = islice(ParameterSampler(search_space, trials, random_state=seed), start, end)
+    best_r2 = best_bacc = -1000
+    best_r2_params = best_bacc_params = None
+    for r2, bacc, params in Parallel(n_jobs=njobs)(delayed(job)(params) for params in sampler):
+        if best_r2 > r2:
+            best_r2 = r2
+            best_r2_params = params
+        if best_bacc > bacc:
+            best_bacc = bacc
+            best_bacc_params = params
+    return best_r2_params.copy(), best_bacc_params.copy(), best_r2, best_bacc
