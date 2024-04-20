68e7edb991cb1db76a3bb51e63a94270bf1a29c4
davips <dpsabc@gmail.com>
2024-04-20 10:26:14 -0300

    make code of l1o similar to l2o Fix broken seeg for some sources due to excessive NaN removal
    shuffle loop
    
    remove age earlier in code

--- experiments/microbiome/regression-inside-l1o---holly.py
+++ experiments/microbiome/regression-inside-l1o---holly.py
@@ -96,17 +96,16 @@ with (sopen(local_cache_uri, ondup="skip") as local_storage, sopen(near_cache_ur
         if noage:
             df.drop(columns=agevar_lst)
 
+        tgt = df[targetvar]
         if source == "deeg":
-            tgt = df[targetvar]
             df.dropna(axis="columns", inplace=True)
-            df[targetvar] = tgt
-            df.dropna(axis="rows", inplace=True)
         elif source == "seeg":
             idx = df.count(axis="columns").sort_values() >= 50
             df = df.loc[idx, :]
             df.dropna(axis="columns", inplace=True)
-        else:
-            df.dropna(axis="rows", inplace=True)
+        df[targetvar] = tgt
+
+        df.dropna(axis="rows", inplace=True)
 
         print(df.shape)
         print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
